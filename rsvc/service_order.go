f2bdd8ab81cc1cc5eea41cf4cf4a007911de9a7e
Bo Maryniuk <bo@maryniuk.net>
2021-06-10 20:27:03 +0200

    Add initial before/after support

--- rsvc/service_order.go
+++ rsvc/service_order.go
@@ -1,5 +1,7 @@
 package rsvc
 
+import "fmt"
+
 type ServiceOrder struct {
 	services []*RunitService
 }
@@ -12,38 +14,122 @@ func NewServiceOrder() *ServiceOrder {
 
 // AddService in order
 func (so *ServiceOrder) AddSevice(service *RunitService) *ServiceOrder {
-	if len(so.services) == 0 {
-		so.services = append(so.services, service)
-	} else {
-		slist := make([]*RunitService, 0)
-		added := false
-
-		if service.conf.After != "" { // Before works only if no after defined
-			for _, current := range so.services {
-				slist = append(slist, current)
-				if current.conf.GetName() == service.conf.After {
-					slist = append(slist, service)
-					added = true
-				}
-			}
-		} else if service.conf.Before != "" {
-			for _, current := range so.services {
-				if current.conf.GetName() == service.conf.Before {
-					slist = append(slist, service)
-					added = true
-				}
-				slist = append(slist, current)
-			}
-		} else {
-			slist = append(slist, so.services...)
-		}
-
-		if !added {
-			slist = append(slist, service)
-		}
-		so.services = slist
-	}
+	so.services = append(so.services, service)
 	return so
+}
+
+func (so *ServiceOrder) popService(idx int) {
+	so.services[len(so.services)-1], so.services[idx] = so.services[idx], so.services[len(so.services)-1]
+	so.services = so.services[:len(so.services)-1]
+}
+
+func (so *ServiceOrder) addBefore(set []*RunitService, service *RunitService) ([]*RunitService, bool) {
+	buff := []*RunitService{}
+	added := false
+	for _, s := range set {
+		if s.conf.GetName() == service.conf.Before {
+			buff = append(buff, service)
+			buff = append(buff, s)
+			added = true
+		} else {
+			buff = append(buff, s)
+		}
+	}
+	return buff, added
+}
+
+func (so *ServiceOrder) addAfter(set []*RunitService, service *RunitService) ([]*RunitService, bool) {
+	buff := []*RunitService{}
+	added := false
+	for _, s := range set {
+		if s.conf.GetName() == service.conf.After {
+			buff = append(buff, s)
+			buff = append(buff, service)
+			added = true
+		} else {
+			buff = append(buff, s)
+		}
+	}
+	return buff, added
+}
+
+func (so *ServiceOrder) Sort() {
+	ordered := []*RunitService{}
+
+	// Add services those have no deps
+	removed := []int{}
+	for idx, service := range so.services {
+		if service.conf.After == "" && service.conf.Before == "" {
+			ordered = append(ordered, service)
+			removed = append(removed, idx)
+		}
+	}
+
+	// Delete moved
+	for _, idx := range removed {
+		so.popService(idx)
+	}
+
+	// Stop, if there are no root services to hook on
+	if len(ordered) == 0 {
+		fmt.Println("There are no independent services found.")
+		return
+	}
+
+	// Add all after.
+	cycler := 0
+	for {
+		dependency := false
+		removed = []int{}
+		for idx, service := range so.services {
+			if service.conf.After != "" {
+				dependency = true
+				var added bool
+				ordered, added = so.addAfter(ordered, service)
+				if added {
+					removed = append(removed, idx)
+				}
+			}
+		}
+		// Delete moved
+		for _, idx := range removed {
+			so.popService(idx)
+		}
+
+		removed = []int{}
+		for idx, service := range so.services {
+			if service.conf.Before != "" {
+				fmt.Println("Found candidate for before:", service.conf.GetName())
+				dependency = true
+				var added bool
+				ordered, added = so.addBefore(ordered, service)
+				if added {
+					removed = append(removed, idx)
+				}
+			}
+		}
+		// Delete moved
+		for _, idx := range removed {
+			so.popService(idx)
+		}
+
+		cycler++
+
+		if len(so.services) == 0 || !dependency {
+			break
+		}
+		if cycler > 10000 {
+			fmt.Println("Service chain is wrong, cannot quit ordering. Giving up...")
+			break
+		}
+	}
+
+	so.services = ordered
+
+	fmt.Println("Services in this scope:")
+	for idx, s := range so.services {
+		fmt.Printf("%d. %s\n", idx+1, s.conf.serviceName)
+	}
 }
 
 // GetServices
