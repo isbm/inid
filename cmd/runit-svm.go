14a3532c74e4c38fd8b11dc3bb7e705925f9846d
Bo Maryniuk <bo@maryniuk.net>
2021-06-11 19:27:53 +0200

    Split mode for sysctl symlink

--- cmd/runit-svm.go
+++ cmd/runit-svm.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path"
+
 	runit_svm "github.com/isbm/runit-svm"
+	"github.com/isbm/runit-svm/sysctl"
 )
 
 func main() {
-	svm := runit_svm.NewSVM()
-	if err := svm.Init(); err != nil { // Look for /etc/runit/rc.d and load the map
-		panic(err)
-	}
+	mode := path.Base(os.Args[0])
+	if mode == "sysctl" {
+		if err := sysctl.NewSvmCtl().Run(); err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+		}
+	} else {
+		// Init system mode
+		svm := runit_svm.NewSVM()
+		if err := svm.Init(); err != nil { // Look for /etc/runit/rc.d and load the map
+			panic(err)
+		}
 
-	svm.Run()
+		svm.Run()
+	}
 }
