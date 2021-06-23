778a9bf3153db615195519c786dacf925957336a
Bo Maryniuk <bo@maryniuk.net>
2021-06-23 16:44:26 +0200

    Remove mounter part

--- rsvc/runservice.go
+++ rsvc/runservice.go
@@ -3,21 +3,16 @@ package rsvc
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"path"
 	"strings"
 
 	"github.com/isbm/inid/rtutils"
-	"github.com/isbm/inid/services/inidmounter"
 	"github.com/isbm/processman"
-	"gopkg.in/yaml.v2"
 )
 
 type RunService struct {
 	serialCommands     []*RunServiceCommand
 	concurrentCommands []*RunServiceCommand
-	conf               *ServiceConfiguration
 	procman            *processman.Processman
 	BaseService
 }
@@ -27,81 +22,26 @@ func NewRunService() *RunService {
 	svc.serialCommands = make([]*RunServiceCommand, 0)
 	svc.concurrentCommands = make([]*RunServiceCommand, 0)
 	svc.procman = processman.New(nil)
+	svc.ref = svc
 
 	return svc
 }
 
-func (svc *RunService) Init(descrPath string) error {
-	buff, err := ioutil.ReadFile(descrPath)
-	if err != nil {
-		return fmt.Errorf("Error reading service description: %s", err.Error())
-	}
-
-	if err := yaml.Unmarshal(buff, &svc.conf); err != nil {
-		return fmt.Errorf("Error parsing service configuration: %s", err.Error())
-	}
-
-	// Set name of the service, taken from the filename, always lowercase
-	svc.conf.SetName(strings.ToLower(strings.Split(path.Base(descrPath), ".")[0]))
-
-	svc.loadConcurrentCommands()
-	svc.loadSerialCommands()
-
-	return nil
-}
-
-func (svc *RunService) loadSerialCommands() *RunService {
+// Called at the end of the main Init()
+func (svc *RunService) postInit() error {
 	for _, command := range svc.conf.GetSerialCommands() {
 		svc.serialCommands = append(svc.serialCommands, NewRunServiceCommand(command).SetConcurrent(false))
 	}
-	return svc
-}
 
-func (svc *RunService) loadConcurrentCommands() *RunService {
 	for _, command := range svc.conf.GetConcurrentCommands() {
 		svc.concurrentCommands = append(svc.concurrentCommands, NewRunServiceCommand(command).SetConcurrent(true))
 	}
-	return svc
-}
 
-func (svc *RunService) GetServiceConfiguration() *ServiceConfiguration {
-	return svc.conf
+	return nil
 }
 
 func (svc *RunService) GetProcesses() map[int]*processman.Process {
 	return svc.procman.Processes()
-}
-
-func (svc *RunService) Start() error {
-	switch svc.GetServiceConfiguration().GetKind() {
-	case SVC_SERVICE:
-		return svc.startService()
-	case SVC_MOUNTER:
-		return svc.startMounter()
-	default:
-		return fmt.Errorf("Unknown service type")
-	}
-}
-
-func (svc *RunService) startMounter() error {
-	log.Printf("Starting mounter service")
-	if svc.conf == nil {
-		return fmt.Errorf("Mounter service was not initialised!")
-	}
-
-	// Start concurrent mounts
-	go func(conf *ServiceConfiguration) {
-		if err := inidmounter.NewInidPremounter(conf.GetConcurrentConfig()).Start(); err != nil {
-			log.Printf("Concurrent mounter %s failed: %s", svc.GetServiceConfiguration().GetName(), err.Error())
-		}
-	}(svc.GetServiceConfiguration())
-
-	// Start serial mounts
-	if err := inidmounter.NewInidPremounter(svc.conf.GetSerialConfig()).Start(); err != nil {
-		log.Printf("Serial mounter %s failed: %s", svc.GetServiceConfiguration().GetName(), err.Error())
-	}
-
-	return nil
 }
 
 func (svc *RunService) formatSTD(p *processman.Process) string {
@@ -129,7 +69,7 @@ func (svc *RunService) formatSTD(p *processman.Process) string {
 	return ""
 }
 
-func (svc *RunService) startService() error {
+func (svc *RunService) Start() error {
 	if svc.conf == nil {
 		return fmt.Errorf("Service was not initialised!")
 	}
